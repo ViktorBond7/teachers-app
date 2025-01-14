ce1b2271037cf06c8cfaa28581b494ef551cb625
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-01-14 20:55:11 +0100

    pages

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import css from "./Home.module.css";
+const HomePage = () => {
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>
+        Unlock your potential with the best language tutors
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
