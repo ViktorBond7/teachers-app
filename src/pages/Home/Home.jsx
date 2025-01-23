faec20cfdc4ecd72e1b80711fb6c381b157efb7b
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-01-23 12:31:39 +0100

    main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import AuthModal from "../../components/AuthModal/AuthModal";
 
 import useAuth from "../../hooks/useAuth";
-// import css from "./Home.module.css";
+import css from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
 const HomePage = () => {
+  const navigate = useNavigate();
   // const [modalType, setModalType] = useState(null); // 'login' or 'signup'
   // const {
   //   user,
@@ -13,9 +15,57 @@ const HomePage = () => {
   //   handleLogout,
   // } = useAuth();
 
+  const handleClick = () => {
+    navigate("/teachers");
+  };
   return (
-    <div className="App">
-      <p>gggggggggggggggggggggggggggggggg</p>
+    <div className={css.containerRoot}>
+      <div className={css.container}>
+        <div className={css.left}>
+          <h1>
+            Unlock your potential with <br /> the best{" "}
+            <span className={css.highlight}>language</span> tutors
+          </h1>
+          <p>
+            Embark on an Exciting Language Journey with Expert Language <br />
+            Tutors. Elevate your language proficiency to new heights by <br />
+            connecting with highly qualified and experienced tutors.
+          </p>
+          <button className={css.btn} onClick={handleClick}>
+            Get started
+          </button>
+        </div>
+        <div className={css.right}>
+          <img src="/public/img/block.png" alt="Emoji with Laptop" />
+        </div>
+      </div>
+
+      <div className={css.stats}>
+        <div className={css.stat}>
+          <span className={css.namber}>32,000+</span>
+          <span className={css.label}>
+            Experienced <br /> tutors
+          </span>
+        </div>
+        <div className={css.stat}>
+          <span className={css.namber}>300,000+</span>
+          <span className={css.label}>
+            5-star tutor <br /> reviews
+          </span>
+        </div>
+        <div className={css.stat}>
+          <span className={css.namber}>120+</span>
+          <span className={css.label}>
+            Subjects <br /> taught
+          </span>
+        </div>
+        <div className={css.stat}>
+          <span className={css.namber}>200+</span>
+          <span className={css.label}>
+            Tutor <br /> nationalities
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
